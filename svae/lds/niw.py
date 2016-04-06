3852b5cc1e0b2f3aaeee13f8fca01e9983eaf4c1
Matthew Johnson <mattjj@csail.mit.edu>
2016-04-06 15:24:23 -0400

    fix niw switched arg order bug, now consistent with mniw change

--- svae/lds/niw.py
+++ svae/lds/niw.py
@@ -9,8 +9,8 @@ import mniw
 # special case of mniw
 
 al2d = np.atleast_2d
-add_dims = lambda A, b, c, d: (A, b[:,None], al2d(c), d)
-remove_dims = lambda A, B, C, d: make_tuple(A, B.ravel(), C[0,0], d)
+add_dims = lambda A, b, c, d: (al2d(c), b[None,:], A, d)
+remove_dims = lambda A, B, C, d: make_tuple(C, B.ravel(), A[0,0], d)
 
 def standard_to_natural(nu, S, m, kappa):
     A, B, C, d = mniw.standard_to_natural(nu, S, m[:,None], al2d(kappa))
@@ -25,3 +25,13 @@ def logZ(natparam):
 def natural_sample(natparam):
     A, Sigma = mniw.natural_sample(add_dims(*natparam))
     return np.ravel(A), Sigma
+
+def expected_standard_params(natparam):
+    J_natural, h, _, _ = expectedstats(natparam)
+    J = -2*J_natural
+
+    mu = np.linalg.solve(J, h)
+    Sigma = np.linalg.inv(J)
+
+    return mu, Sigma
+    # return natural_sample(natparam)

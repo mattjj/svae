85751891a0f0704d0c4e7a862a0fd9dbee09cd7d
Matthew Johnson <mattjj@csail.mit.edu>
2016-10-28 11:32:35 -0700

    add dependencies to setup.py file

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ import numpy as np
 from Cython.Build import cythonize
 
 setup(
+    name='svae',
+    author='Matthew James Johnson and David Duvenaud',
+    author_email='mattjj@csail.mit.edu, duvenaud@cs.toronto.edu',
+    packages=['svae', 'autograd_linalg'],
     ext_modules=cythonize('**/*.pyx'),
     include_dirs=[np.get_include(),],
+    install_requires=['autograd >= 1.1.7', 'numpy >= 1.10.0', 'cython >= 0.24',
+                      'scipy >= 0.18.1'],
+    license='MIT',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 2.7'],
 )
